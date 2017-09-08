ef95142da46d665c1070c895862af8b5a6e688e6
wangqiang <wangqiangweb@163.com>
2017-09-08 16:12:15 +0800

    滚动加载更多

--- app/components/LoadMore/index.jsx
+++ app/components/LoadMore/index.jsx
@@ -16,9 +16,33 @@ class LoadMore extends React.Component {
         this.props.loadMoreFn()
     }
 
+    componentDidMount() {
+        //截流
+        let timeoutId;
+        const loadMoreFn = this.props.loadMoreFn;
+        const wrapper = this.refs.wrapper;
+        function callback() {
+           const top = wrapper.getBoundingClientRect().top;//获取DOM元素距离顶部的距离
+            const windowHeight = window.screen.height;
+            if(top && top < windowHeight){
+                // 当wrapper 已经被滚动到暴露在页面的可视范围之内的时候，触发
+                loadMoreFn()
+            }
+        }
+        window.addEventListener('scroll',function(){
+            if(this.props.isLoadingMore){
+                return
+            }
+            if(timeoutId){
+                clearTimeout(timeoutId)
+            }
+            timeoutId = setTimeout(callback,50)
+        }.bind(this),false)
+    }
+
     render() {
         return (
-            <div className="load-more">
+            <div className="load-more" ref="wrapper">
                 {
                     this.props.isLoadingMore
                         ? <span>加载中...</span>
